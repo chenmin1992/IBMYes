40d8189384ff04e79138ed2a090cdb77a6c4cd03
chenmin <chenmin.dba@haodai.net>
2020-09-04 15:21:37 +0800

    Some optimization

--- install.sh
+++ install.sh
@@ -3,75 +3,81 @@ SH_PATH=$(cd "$(dirname "$0")";pwd)
 cd ${SH_PATH}
 
 create_mainfest_file(){
-    echo "进行配置。。。"
-    read -p "请输入你的应用名称：" IBM_APP_NAME
-    echo "应用名称：${IBM_APP_NAME}"
-    read -p "请输入你的应用内存大小(默认256)：" IBM_MEM_SIZE
-    if [ -z "${IBM_MEM_SIZE}" ];then
-    IBM_MEM_SIZE=256
+    echo "进行配置..."
+    read -p "请输入你的应用名称: " IBM_APP_NAME
+    if [ -z "${IBM_APP_NAME}" ]; then
+        echo '未填写应用名称, 退出'
+        exit 1
     fi
-    echo "内存大小：${IBM_MEM_SIZE}"
+    echo "应用名称: ${IBM_APP_NAME}"
+    echo "正在获取内存大小..."
+    IBM_MEM_SIZE=$(ibmcloud cf app ${IBM_APP_NAME} | grep 'memory usage' | awk '{print $3}')
+    if [ -z "${IBM_MEM_SIZE}" ]; then
+        IBM_MEM_SIZE='256M'
+    fi
+    echo "内存大小: ${IBM_MEM_SIZE}"
     UUID=$(cat /proc/sys/kernel/random/uuid)
-    echo "生成随机UUID：${UUID}"
-    WSPATH=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
-    echo "生成随机WebSocket路径：${WSPATH}"
-    
-    cat >  ${SH_PATH}/IBMYes/v2ray-cloudfoundry/manifest.yml  << EOF
+    echo "生成随机UUID: ${UUID}"
+    WSPATH=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6)
+    echo "生成随机WebSocket路径: ${WSPATH}"
+
+    cat > ${SH_PATH}/IBMYes/v2ray-cloudfoundry/manifest.yml << EOF
     applications:
     - path: .
       name: ${IBM_APP_NAME}
       random-route: true
-      memory: ${IBM_MEM_SIZE}M
+      memory: ${IBM_MEM_SIZE}
 EOF
 
-    cat >  ${SH_PATH}/IBMYes/v2ray-cloudfoundry/v2ray/config.json  << EOF
-    {
-        "inbounds": [
-            {
-                "port": 8080,
-                "protocol": "vmess",
-                "settings": {
-                    "clients": [
-                        {
-                            "id": "${UUID}",
-                            "alterId": 4
-                        }
-                    ]
-                },
-                "streamSettings": {
-                    "network":"ws",
-                    "wsSettings": {
-                        "path": "${WSPATH}"
+    cat > ${SH_PATH}/IBMYes/v2ray-cloudfoundry/v2ray/config.json << EOF
+{
+    "inbounds":[
+        {
+            "port":8080,
+            "protocol":"vmess",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"${UUID}",
+                        "alterId":4
                     }
+                ]
+            },
+            "streamSettings":{
+                "network":"ws",
+                "wsSettings":{
+                    "path":"${WSPATH}"
                 }
             }
-        ],
-        "outbounds": [
-            {
-                "protocol": "freedom",
-                "settings": {}
+        }
+    ],
+    "outbounds":[
+        {
+            "protocol":"freedom",
+            "settings":{
+
             }
-        ]
-    }
+        }
+    ]
+}
 EOF
-    echo "配置完成。"
+    echo "配置完成."
 }
 
 clone_repo(){
-    echo "进行初始化。。。"
-    git clone https://github.com/CCChieh/IBMYes
-    cd IBMYes
-    git submodule update --init --recursive
-    cd v2ray-cloudfoundry/v2ray
+    echo "进行初始化..."
+    rm -rf IBMYes
+    git clone https://github.com/chenmin1992/IBMYes
+    cd IBMYes/v2ray-cloudfoundry/v2ray
+
     # Upgrade V2Ray to the latest version
-    rm v2ray v2ctl
-    
+    rm v2ray v2ctl geoip.dat geosite.dat
     # Script from https://github.com/v2fly/fhs-install-v2ray/blob/master/install-release.sh
     # Get V2Ray release version number
     TMP_FILE="$(mktemp)"
     if ! curl -s -o "$TMP_FILE" 'https://api.github.com/repos/v2fly/v2ray-core/releases/latest'; then
         rm "$TMP_FILE"
-        echo 'error: 获取最新V2Ray版本号失败。请重试'
+        echo 'error: 获取最新V2Ray版本号失败, 请重试'
         exit 1
     fi
     RELEASE_LATEST="$(sed 'y/,/\n/' "$TMP_FILE" | grep 'tag_name' | awk -F '"' '{print $4}')"
@@ -85,39 +91,41 @@ clone_repo(){
     fi
     unzip latest-v2ray.zip v2ray v2ctl geoip.dat geosite.dat
     rm latest-v2ray.zip
-    
+
     chmod 0755 ./*
     cd ${SH_PATH}/IBMYes/v2ray-cloudfoundry
-    echo "初始化完成。"
+    echo "初始化完成."
 }
 
 install(){
-    echo "进行安装。。。"
+    echo "进行安装..."
     cd ${SH_PATH}/IBMYes/v2ray-cloudfoundry
     ibmcloud target --cf
     ibmcloud cf install
+    add=$(ibmcloud cf routes | grep klutztech | awk '{print $2"."$3}')
     ibmcloud cf push
-    echo "安装完成。"
-    echo "生成的随机 UUID：${UUID}"
-    echo "生成的随机 WebSocket路径：${WSPATH}"
+    echo "安装完成."
+    echo "地址: ${add}"
+    echo "生成的随机 UUID: ${UUID}"
+    echo "生成的随机 WebSocket路径: ${WSPATH}"
     VMESSCODE=$(base64 -w 0 << EOF
-    {
-      "v": "2",
-      "ps": "ibmyes",
-      "add": "ibmyes.us-south.cf.appdomain.cloud",
-      "port": "443",
-      "id": "${UUID}",
-      "aid": "4",
-      "net": "ws",
-      "type": "none",
-      "host": "",
-      "path": "${WSPATH}",
-      "tls": "tls"
-    }
+{
+  "v": "2",
+  "ps": "${IBM_APP_NAME}",
+  "add": "${add}",
+  "port": "443",
+  "id": "${UUID}",
+  "aid": "4",
+  "net": "ws",
+  "type": "none",
+  "host": "",
+  "path": "${WSPATH}",
+  "tls": "tls"
+}
 EOF
     )
-	echo "配置链接："
-    echo vmess://${VMESSCODE}
+	echo "配置链接: "
+    echo "vmess://${VMESSCODE}"
 
 }
 
